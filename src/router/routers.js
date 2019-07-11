962be6500edb16eeb63d7f22e21ee4e243faa160
wx463736788 <wx463736788@outlook.com>
2019-07-11 09:31:09 +0800

    提交

---
+++ src/router/routers.js
@@ -0,0 +1,24 @@
+import layout from '@/layout'
+
+const route = [
+  {
+    path: '/',
+    name: '_home',
+    // redirect: '/',
+    component: layout,
+    children: [
+      {
+        path: '/',
+        name: 'home_page',
+        meta: {
+          title: '首页',
+          husband: '1',
+          name: '1-1'
+        },
+        component: () => import('@/components/HelloWorld.vue')
+      }
+    ]
+  }
+]
+
+export default route
